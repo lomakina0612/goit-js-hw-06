c20e4fbda3d8e10abf10019c5ad4da32009c6269
Elena Lomakina <lomakina0612@gmail.com>
2022-08-08 01:23:29 +0200

    task 3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,9 +1,11 @@
 // Задание 3
 
-// Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
+// Напиши скрипт для создания галереи изображений по массиву данных. 
+// В HTML есть список ul.gallery.
 
 // <ul class="gallery"></ul>
-// Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. 
+// Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
 
 // Все элементы галереи должны добавляться в DOM за одну операцию вставки.
 // Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
@@ -36,3 +38,22 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.classList.add("flex");
+
+// через массив <li>
+// const items = images.map((image) => {
+//   return `<li><img src="${image.url}" alt="${image.alt}" class="img"></li>`;
+// })
+
+// items.forEach((item) => {
+//   galleryEl.insertAdjacentHTML("beforeend", item);
+// })
+
+//одной строкой
+const galleryInnerHtml = images.reduce((acc, image) => {
+  return acc += `<li><img src="${image.url}" alt="${image.alt}" class="img"></li>`;
+}, "");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryInnerHtml);
